960922923cb83633fceeefdfb6e0be0bdc6afe4f
Oscar Ortiz <ortizg.oscar@gmail.com>
2021-05-22 13:08:27 -0300

    image helpers and linkk to sow post show page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,28 @@
 module ApplicationHelper
+
+  def sample_watch_image
+    %w[
+        https://images.pexels.com/photos/47856/rolex-wrist-watch-clock-gmt-47856.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500
+        https://images.pexels.com/photos/7617108/pexels-photo-7617108.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500
+        https://images.pexels.com/photos/5895645/pexels-photo-5895645.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500
+        https://images.pexels.com/photos/4801853/pexels-photo-4801853.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500
+        https://images.pexels.com/photos/5443583/pexels-photo-5443583.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500
+        https://images.pexels.com/photos/5895642/pexels-photo-5895642.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500
+        https://images.pexels.com/photos/5895646/pexels-photo-5895646.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500
+        https://images.pexels.com/photos/128206/pexels-photo-128206.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500
+        https://images.pexels.com/photos/266666/pexels-photo-266666.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500
+        https://images.pexels.com/photos/190819/pexels-photo-190819.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500
+        https://images.pexels.com/photos/125779/pexels-photo-125779.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500
+        https://images.pexels.com/photos/380782/pexels-photo-380782.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500
+        https://images.pexels.com/photos/3766111/pexels-photo-3766111.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500
+        https://images.pexels.com/photos/3651834/pexels-photo-3651834.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500
+        https://images.pexels.com/photos/2442893/pexels-photo-2442893.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500
+        https://images.pexels.com/photos/2155319/pexels-photo-2155319.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500
+        https://images.pexels.com/photos/6166165/pexels-photo-6166165.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500
+        https://images.pexels.com/photos/6166175/pexels-photo-6166175.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500
+        https://images.pexels.com/photos/6166174/pexels-photo-6166174.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500
+        https://images.pexels.com/photos/6328840/pexels-photo-6328840.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500
+        https://images.pexels.com/photos/1697215/pexels-photo-1697215.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500
+    ].sample
+  end
 end
