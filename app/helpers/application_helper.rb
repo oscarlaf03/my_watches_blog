242bac749771f205fea29873aa9b60d19d1b7b54
Oscar Ortiz <ortizg.oscar@gmail.com>
2021-05-23 13:30:22 -0300

    added some images

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -23,6 +23,16 @@ module ApplicationHelper
         https://images.pexels.com/photos/6166174/pexels-photo-6166174.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500
         https://images.pexels.com/photos/6328840/pexels-photo-6328840.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500
         https://images.pexels.com/photos/1697215/pexels-photo-1697215.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500
+        https://hodinkee.imgix.net/uploads/images/cca09692-ba32-4461-a3df-6909447432e2/Rectangle6.jpg?ixlib=rails-1.1.0&fm=png&q=55&auto=format&usm=12&fit=crop&w=1446
+        https://hodinkee.imgix.net/uploads/images/7bd4f901-96ff-4727-ad8f-39b78a7d0395/3S3A0089.jpg?ixlib=rails-1.1.0&fm=png&q=55&auto=format&usm=12&fit=crop&w=798
+        https://hodinkee.imgix.net/uploads/images/3f5636e7-12ad-4d90-a7e6-6023c0c1002c/3S3A0089-1.jpg?ixlib=rails-1.1.0&fm=png&q=55&auto=format&usm=12&fit=crop&w=798
+        https://hodinkee.imgix.net/uploads/images/3ea3f078-e0fc-4dd3-9fed-3b4d136547dc/picasso-watch-hero.jpg?ixlib=rails-1.1.0&fm=jpg&q=55&auto=format&usm=12&fit=crop&ch=Width%2CDPR%2CSave-Data&alt=
+        https://hodinkee.imgix.net/uploads/images/8f41e39f-3568-4177-88d0-f56a85a6346d/heroKT1?ixlib=rails-1.1.0&fm=jpg&q=55&auto=format&usm=12&fit=crop&ch=Width%2CDPR%2CSave-Data&alt=
+        https://hodinkee.imgix.net/uploads/images/41897ee2-8061-46d1-a498-50893eb1811e/KT1.jpg?ixlib=rails-1.1.0&fm=jpg&q=55&auto=format&usm=12&ch=Width%2CDPR%2CSave-Data&fit=crop&w=700
+        https://hodinkee.imgix.net/uploads/images/44549eba-473f-4990-be42-c0a939f25381/Kurono-Tokyo-Toki-4.jpg?ixlib=rails-1.1.0&fm=jpg&q=55&auto=format&usm=12&ch=Width%2CDPR%2CSave-Data&fit=crop&w=820
+        https://hodinkee.imgix.net/uploads/images/5f0eebd4-5317-49ec-8214-a68b9687bc6b/kt2?ixlib=rails-1.1.0&fm=jpg&q=55&auto=format&usm=12&ch=Width%2CDPR%2CSave-Data&fit=crop&w=700
+        https://hodinkee.imgix.net/uploads/images/5011ddc9-e155-4635-b4c0-216be2c452c9/Hublot-big-bang-unico-sorai-green-hero.jpg?ixlib=rails-1.1.0&fm=jpg&q=55&auto=format&usm=12&fit=crop&ch=Width%2CDPR%2CSave-Data&alt=
+        https://hodinkee.imgix.net/uploads/images/b3bdf989-c86e-4040-9690-c4632f27afcb/Hublot-big-bang-unico-sorai-green-8.jpg?ixlib=rails-1.1.0&fm=jpg&q=55&auto=format&usm=12&ch=Width%2CDPR%2CSave-Data&fit=crop&w=820
     ].sample
   end
 end
