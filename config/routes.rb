60d42b59bdeb633ca5fb265a963ede11baee2946
Oscar Ortiz <ortizg.oscar@gmail.com>
2021-05-25 19:30:06 -0300

    fixed forms for edit password

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,7 @@ Rails.application.routes.draw do
 
   root to: 'posts#index'
 
-  get 'users/:handle', to: 'users#show', as: :user, constraints: { :handle => %r{[^\/]+} }
-  resources :users, param: :handle, constraints: { :handle => %r{[^\/]+} }, only:[:edit, :update,:index,:destroy]
+  resources :users, param: :handle, constraints: { :handle => %r{[^\/]+} }, except: [:create, :new]
   get 'sponsors/posts', to: 'remote_posts#index'
 
 
