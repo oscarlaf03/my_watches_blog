3826b8f50583cf70ddf7e113b1d610ae8d68f2c2
Oscar Ortiz <ortizg.oscar@gmail.com>
2021-07-11 16:25:03 -0300

    created new page and opened up w/o auth

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,12 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home ]
+  skip_before_action :authenticate_user!, only: [ :home, :new_page ]
+  skip_after_action :verify_authorized, only: [ :home, :new_page ]
 
   def home
   end
+
+  def new_page
+
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,6 @@ Rails.application.routes.draw do
 
   resources :users, param: :handle, constraints: { :handle => %r{[^\/]+} }, except: [:create, :new]
   get 'sponsors/posts', to: 'remote_posts#index'
-
+  get 'new_page', to: 'pages#new_page'
 
 end
